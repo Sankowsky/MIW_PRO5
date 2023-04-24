7726b09b930b910a98048a224d4bdb51f8150870
Sankowsky <bartek.sankowski11@gmail.com>
2023-04-25 00:06:32 +0200

    importy

---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+from tensorflow.keras.utils import to_categorical
+
